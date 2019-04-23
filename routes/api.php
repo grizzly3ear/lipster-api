eb350c0dcc8e4d01101498fbbac75efb39cf8342
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-23 16:07:48 +0700

    [UDPATE] remove / in route to avoid misunderstand

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 Route::group(['prefix' => 'lipstick'], function () { // localhost/api/lipstick/...
-    Route::get('/', 'LipstickController@getAll');
+    Route::get('', 'LipstickController@getAll');
     Route::get('{id}', 'LipstickController@getLipstickById');
     Route::post('', 'LipstickController@storeLipstick');
     Route::delete('{id}', 'LipstickController@deleteLipstickId');
