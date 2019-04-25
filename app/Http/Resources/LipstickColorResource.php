544ecc817ebade13471b93c72e3ec15ac07650da
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-04-25 19:32:47 +0700

    [UPDATE] add attribute to lipstickColorResource

--- app/Http/Resources/LipstickColorResource.php
+++ app/Http/Resources/LipstickColorResource.php
@@ -15,11 +15,15 @@ class LipstickColorResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this-> id,
-            'color_name' => $this -> color_name,
-            'rgb' => $this-> rgb,
-            'color_code' => $this-> color_code,
-            'image' => LipstickImageResource::collection($this-> lipstickImages)
+            'id' => $this->id,
+            'color_name' => $this->color_name,
+            'rgb' => $this->rgb,
+            'color_code' => $this->color_code,
+            'image' => LipstickImageResource::collection($this->lipstickImages),
+            'detail' => $this->lipstickDetail->description
         ];
     }
 }
+
+
+
