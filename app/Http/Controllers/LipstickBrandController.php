a8e99b33de413702d6bbb42c5ddb7bf356140304
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-05-09 11:04:15 +0700

    [UPDATE] remove useless methods

--- app/Http/Controllers/LipstickBrandController.php
+++ app/Http/Controllers/LipstickBrandController.php
@@ -34,35 +34,12 @@ class LipstickBrandController extends Controller
         // return $user;
     }
 
-    public function getLipstickById($id) {
-        return new LipstickColorResource(LipstickColor::find($id));
-    }
-
     public function getLipstickByBrand($brand_id){
         $brand = LipstickBrand::find($brand_id);
         return new LipstickBrandResource($brand);
     }
 
-    public function getLipstickByDetail($detail_id){
-        $detail = LipstickDetail::find($detail_id);
-        return new LipstickDetailResource($detail);
-    }
-
-    // public function storeLipstick(Request $request){
-    //     $lipstick = $this -> mapRequestToLipstickBrand($request);
-    //     $lipstick -> save();
-
-    //     return $lipstick;
-    // }
-
-    // public function mapRequestToLipstickBrand(Request $request){
-    //     $lipstick = new LipstickBrand();
-    //     $lipstick -> brand = $request -> brand;
-
-    //     return $lipstick;
-    // }
-
-    public function storeLipstick(Request $request){
+    public function storeLipstickBrand(Request $request){
         $brand = new LipstickBrand();
         $brand -> name = $request -> name;
         $brand -> save();
@@ -103,59 +80,8 @@ class LipstickBrandController extends Controller
         return $lipstick;
     }
 
-    public function editLipstickDetail(Request $request,$id){
-        $lipstick = LipstickDetail::find($id);
-        $lipstick -> name = $request -> name;
-        $lipstick -> max_price = $request -> max_price;
-        $lipstick -> min_price = $request -> min_price;
-        $lipstick -> type = $request -> type;
-        $lipstick -> opacity = $request -> opacity;
-        $lipstick -> description = $request -> description;
-        $lipstick -> composition = $request -> composition;
-        $lipstick -> apply = $request -> apply;
-        $lipstick -> lipstick_brand_id = $request -> lipstick_brand_id;
-        $lipstick -> save();
-
-        return $lipstick;
-    }
-
-    public function editLipstickColor(Request $request, $id){
-        $lipstick = LipstickColor::find($id);
-        $lipstick -> color_name = $request -> color_name;
-        $lipstick -> rgb = $request -> rgb;
-        $lipstick -> color_code = $request -> color_code;
-        $lipstick -> lipstick_detail_id = $request -> lipstick_detail_id;
-        $lipstick -> save();
-
-        return $lipstick;
-    }
-
-    public function editLipstickImage(Request $request, $id){
-        $lipstick = LipstickImage::find($id);
-        $lipstick -> image = $request -> image;
-        $lipstick -> lipstick_color_id = $request -> lipstick_color_id;
-        $lipstick -> save();
-
-        return $lipstick;
-    }
-
-    public function deleteLipstickId($id){
-        $lipstick = LipstickColor::find($id);
-        $lipstick -> delete();
-    }
-
     public function deleteLipstickBrand($id){
         $lipstick = LipstickBrand::find($id);
-        $lipstick -> delete();
-    }
-
-    public function deleteLipstickDetail($id){
-        $lipstick = LipstickDetail::find($id);
-        $lipstick -> delete();
-    }
-
-    public function deleteLipstickImage($id){
-        $lipstick = LipstickImage::find($id);
-        $lipstick -> delete();
+        $lipstick->delete();
     }
 }
