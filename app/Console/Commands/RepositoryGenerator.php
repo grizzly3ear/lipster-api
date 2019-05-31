8106f15a6a0da140bc97bfe42350b161b51dbab2
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-05-31 17:05:46 +0700

    [UPDATE] change fix repository generator route filter don't work

--- app/Console/Commands/RepositoryGenerator.php
+++ app/Console/Commands/RepositoryGenerator.php
@@ -107,10 +107,10 @@ class RepositoryGenerator extends Command
 
         $route = "\nRoute::group(['prefix' => ''], function () {";
         $route = $route."\n\tRoute::get('', '{$name}Controller@getAll{$name}');";
-        $route = $route."\n\tRoute::get('{{$lowerName}_id}', '{$name}Controller@get{$name}ById')->where('{$lowerName}', '[0-9]+');";
+        $route = $route."\n\tRoute::get('{{$lowerName}_id}', '{$name}Controller@get{$name}ById')->where('{$lowerName}_id', '[0-9]+');";
         $route = $route."\n\tRoute::post('', '{$name}Controller@create{$name}');";
-        $route = $route."\n\tRoute::put('{{$lowerName}_id}', '{$name}Controller@update{$name}ById')->where('{$lowerName}', '[0-9]+');";
-        $route = $route."\n\tRoute::delete('{{$lowerName}_id}', '{$name}Controller@delete{$name}ById')->where('{$lowerName}', '[0-9]+');";
+        $route = $route."\n\tRoute::put('{{$lowerName}_id}', '{$name}Controller@update{$name}ById')->where('{$lowerName}_id', '[0-9]+');";
+        $route = $route."\n\tRoute::delete('{{$lowerName}_id}', '{$name}Controller@delete{$name}ById')->where('{$lowerName}_id', '[0-9]+');";
         $route = $route."\n});\n";
 
         return $route;
