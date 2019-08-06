569b1624532c8aea0ab862b90c24467c715b0699
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-08-06 10:03:27 +0700

    [ADD] trend group

---
+++ app/Models/TrendGroup.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrendGroup extends Model
+{
+    protected $table = 'trend_group';
+    protected $primaryKey = 'id';
+}
