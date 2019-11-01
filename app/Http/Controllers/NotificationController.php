21d8b50b0437e4baabdb5adf795fc0c7354d4a1d
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-11-01 20:27:57 +0700

    [UPDATE] test notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,6 +9,11 @@ use App\Http\Resources\NotificationResource;
 use App\Repositories\NotificationRepository;
 use App\Models\Notification;
 
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use FCM;
+
 class NotificationController extends Controller
 {
     protected $notificationRepository;
@@ -18,6 +23,26 @@ class NotificationController extends Controller
     }
 
     public function getAllNotification () {
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60*20);
+
+        $notificationBuilder = new PayloadNotificationBuilder('Hi');
+        $notificationBuilder->setBody('Bank!!!')
+                            ->setSound('default');
+
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData(['a_data' => 'my_data']);
+
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        $token = "fhxcTOC15Vs:APA91bGvGDm06l-ZxWQQvL89VEShLX_NtqdoJJW_ACTfEBZSUOVscY7tjHIT_8s9ST6dvS0CNw9uw54G3Pr6NftW08fiXckOOUbShZwq2nrmhByZOdCtj-oIKRA5HSPknNTqidNi71-D";
+
+        $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+
+        $downstreamResponse->numberSuccess();
+
         $notifications = $this->notificationRepository->findAll();
 
         return NotificationResource::collection($notifications);
