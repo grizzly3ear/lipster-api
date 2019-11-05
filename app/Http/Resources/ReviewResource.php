e07beedd881ac28a61b668f77ca7f5faac01dac9
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-11-05 23:18:33 +0700

    [UPDATE] show review by user

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -14,14 +14,16 @@ class ReviewResource extends JsonResource
      */
     public function toArray($request)
     {
+
+
         return [
             'id' => $this->pivot->id,
-            // 'user' => new UserResource($this->user),
             'comment' => $this->pivot->comment,
             'skin_color' => $this->pivot->skin_color,
             'rating' => $this->pivot->rating,
             'created_at' => $this->pivot->created_at,
             'updated_at' => $this->pivot->updated_at,
+            'lipstick_color' => new LipstickColorResource($this),
 
         ];
     }
