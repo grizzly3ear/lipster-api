f7bb4dc697322cc5cf34a3d0ebd5b7cfaf42044e
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-11-12 15:53:40 +0700

    [UPDATE]  dataBuilder send trend group id

--- app/Repositories/NotificationRepository.php
+++ app/Repositories/NotificationRepository.php
@@ -91,7 +91,7 @@ class NotificationRepository implements NotificationRepositoryInterface
 
 
         $dataBuilder = new PayloadDataBuilder();
-        $dataBuilder->addData(['a_data' => 'my_data']);
+        $dataBuilder->addData([$name."_data" => $model->id]);
 
         $option = $optionBuilder->build();
         $notification = $notificationBuilder->build();
