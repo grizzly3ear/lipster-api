ebd67159bb413ee8bd6c9d3d85f583b18b847c74
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-11-18 01:20:44 +0700

    [UPDATE] sort trend group by create at

--- app/Models/TrendGroup.php
+++ app/Models/TrendGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class TrendGroup extends Model
@@ -9,6 +10,16 @@ class TrendGroup extends Model
     protected $table = 'trend_group';
     protected $primaryKey = 'id';
     protected $fillable = ['name', 'image', 'description', 'release_date', 'release', 'force'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Order by name ASC
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
 
     public function trends() {
         return $this->hasMany(Trend::class, 'trend_group_id');
